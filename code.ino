f5fb475b7337503c233b6f079f5746cf11b2fc9b
anushaupadhya <73431941+anushaupadhya@users.noreply.github.com>
2021-07-15 17:23:08 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -3,10 +3,14 @@ const int trigPin = 9;
 const int echoPin = 10;
 const int buzzer = 11;
 const int ledPin = 13;
+int sensor = A1;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0; 
 
 long duration;
 int distance;
 int safetyDistance;
+
 
 
 void setup() {
@@ -14,11 +18,32 @@ pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);
 pinMode(buzzer, OUTPUT);
 pinMode(ledPin, OUTPUT);
+pinMode(sensor, INPUT);
 Serial.begin(9600);
 }
 
 
 void loop() {
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(ledPin, HIGH); // turn LED ON
+     digitalWrite(buzzer, HIGH);
+    delay(500);                // delay 500 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(ledPin, LOW); // turn LED OFF
+      delay(500);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
 
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
@@ -31,9 +56,10 @@ duration = pulseIn(echoPin, HIGH);
 distance= duration*0.034/2;
  
 safetyDistance = distance;
-if(safetyDistance <10){
+if(safetyDistance <25){
   digitalWrite(buzzer, HIGH);
   digitalWrite(ledPin, HIGH);
+  delay(500);
 }
 else{
   digitalWrite(buzzer, LOW);
@@ -45,3 +71,6 @@ else{
  Serial.print("Distance:");
  Serial.println(distance);
 }
+
+  
+
